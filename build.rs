ee3eff392e210c426c85748a95f534a5c1ebf91e
Bruno-sm <bru.1bruno@gmail.com>
2018-07-12 19:22:26 +0200

    Add MuPDF compilation to the build system

--- build.rs
+++ build.rs
@@ -2,8 +2,25 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
+    Command::new("git").arg("clone")
+                       .arg("--recursive")
+                       .arg("git://git.ghostscript.com/mupdf.git")
+                       .spawn()
+                       .expect("Failed to clone MuPDF")
+                       .wait()
+                       .expect("Failed to wait for git process");
+    Command::new("make").arg("-C")
+                        .arg("mupdf")
+                        .arg("HAVE_X11=no")
+                        .arg("HAVE_GLUT=no")
+                        .spawn()
+                        .expect("Failed to compile MuPDF")
+                        .wait()
+                        .expect("Failed to wait for make process");
+
     println!("cargo:rustc-link-lib=static=mupdf");
     println!("cargo:rustc-link-lib=static=mupdf-third");
     println!("cargo:rustc-link-lib=static=mupdf-threads");
